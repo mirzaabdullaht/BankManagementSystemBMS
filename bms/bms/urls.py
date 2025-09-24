eec736717e6527d451be9346e6fa6b81ff0cfb2a
Mirza Abdullah Tariq <mirzaabdullaht@outlook.com>
2025-09-24 23:34:24 +0500

    Initial Commit for phase 3 and 6

--- bms/bms/urls.py
+++ bms/bms/urls.py
@@ -21,6 +21,7 @@ from django.urls import path
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('banks.urls')),
